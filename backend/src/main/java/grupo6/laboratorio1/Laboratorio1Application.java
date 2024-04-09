6c73d59a26892b6df9d04a5ee9f29418329f8ae1
unknown <ignacioladal@gmail.com>
2024-04-09 18:52:40 -0400

    feat: agregar proyecto de java springboot para el backend del repositorio

---
+++ backend/src/main/java/grupo6/laboratorio1/Laboratorio1Application.java
@@ -0,0 +1,13 @@
+package grupo6.laboratorio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/grupo6/laboratorio1/Laboratorio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo6.laboratorio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
