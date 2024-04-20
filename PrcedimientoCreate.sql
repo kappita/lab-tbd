9497679617fff538ce19ab4ffcf3109b38caf24b
javiera <javiera.godoy.r@usach.cl>
2024-04-20 14:59:09 -0400

    Corrección procedimiento almacenado

--- PrcedimientoCreate.sql
+++ PrcedimientoCreate.sql
@@ -1,38 +1,38 @@
+-- Crear la tabla para almacenar los resultados del reporte de actividad por usuario --
+CREATE TABLE IF NOT EXISTS usuario_contador_operaciones (
+    id serial PRIMARY KEY,
+    username character varying UNIQUE,
+    total_operations bigint,
+    last_query timestamp  -- Agregar una columna para almacenar la fecha de la última consulta
+);
+
 -- Procedimiento almacenado para generar el reporte de actividad por usuario y guardar los resultados en una tabla --
-CREATE OR REPLACE FUNCTION generate_user_query_report_and_save()
+CREATE OR REPLACE FUNCTION generar_reporte_operaciones()
 RETURNS VOID AS $$
 BEGIN
-    -- Crear la tabla para almacenar los resultados si no existe
-    CREATE TABLE IF NOT EXISTS user_query_report (
-        id serial PRIMARY KEY,
-        username character varying,
-        query_count bigint,
-        last_query timestamp,
-        query text,
-        operacion varchar(10)
-    );
-
-    -- Eliminar cualquier dato previo en la tabla
-    TRUNCATE TABLE user_query_report;
-
     -- Insertar los resultados del reporte en la tabla
-    INSERT INTO user_query_report (username, query_count, last_query, query, operacion)
+    INSERT INTO usuario_contador_operaciones (username, total_operations, last_query)
     SELECT
         ra.nombre_usuario,
-        COUNT(ra.id) AS query_count,
-        MAX(ra.timestamp) AS last_query,
-        ra.query,
-        ra.operacion
+        COUNT(*) AS total_operations,
+        MAX(ra.timestamp) AS last_query
     FROM
         registro_actividad ra
     WHERE
         ra.operacion IN ('INSERT', 'UPDATE', 'DELETE')
     GROUP BY
-        ra.nombre_usuario, ra.query, ra.operacion
-    ORDER BY
-        query_count DESC; -- Ordenar por query_count de mayor a menor
-
+        ra.nombre_usuario
+    ON CONFLICT (username) DO UPDATE
+    SET total_operations = EXCLUDED.total_operations,
+        last_query = EXCLUDED.last_query;
 END;
 $$ LANGUAGE plpgsql;
 
--- PTesteo: SELECT * FROM user_query_report;
+
+-- Cada vez que se realiza una operación
+-- primero se ejecuta esto
+--SELECT generar_reporte_operaciones();
+
+
+-- y luego se consulta el reporte
+-- SELECT * FROM usuario_contador_operaciones;
