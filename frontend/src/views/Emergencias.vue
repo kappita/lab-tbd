d7c2fb668a0ea3faa2ed89fe315eb13a94176d34
unknown <ignacioladal@gmail.com>
2024-04-21 14:44:02 -0400

    feat: cambio de stack en el frontend, de Nuxtjs a Vue3 puro

---
+++ frontend/src/views/Emergencias.vue
@@ -0,0 +1,72 @@
+<script setup>
+  import Emergency from '@/components/Emergency.vue';
+  import { ref } from 'vue';
+  const email = ref("")
+  const emergencies = [
+    {emergency_name: "El pepe", emergency_id: 10, skills: ["bañarse", "ser sigma"]},
+    {emergency_name: "Ete sech", emergency_id: 11, skills: ["Ser skibidi", "ser Toilet"]}
+  ]
+</script>
+
+<template>
+  <main>
+    <header>
+      <h1>
+        Listado de emergencias activas
+      </h1>
+      <h2>
+        Inscríbete como disponible para ayudar
+      </h2>
+    </header>
+
+    <div class="emergency-section">
+      <div class="user-input">
+        <h2>
+          Ingresa tu correo electrónico para inscribirte en emergencias:
+        </h2>
+        <input placeholder="Correo electrónico. Ej: rene@outlook.my" type="text" name="email" id="" v-model="email">
+      </div>
+
+      <div class="emergencies">
+        <Emergency v-for="e in emergencies" :data="e"></Emergency>
+      </div>
+    </div>
+
+
+  </main>
+
+</template>
+
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  padding-left: 10%;
+  padding-right: 10%;
+}
+
+header {
+  height: 20%;
+  width: 100%;
+}
+
+.emergency-section {
+  height: 80%;
+  width: 100%;
+}
+
+.user-input {
+  height: 20%;
+  width: 100%;
+}
+
+.emergencies {
+  display: grid;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  width: 50%;
+  height: 80%;
+  overflow: scroll;
+}
+
+</style>
---
+++ frontend/src/views/Registro.vue
@@ -0,0 +1,67 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+  const usertype = ref(0)
+  const name = ref("")
+  const email = ref("")
+  const password = ref("")
+
+  const sendForm = () => {
+    if (usertype.value === 0) {
+      window.alert("Información inválida")
+      return
+    }
+    const body = {
+      nombre: name.value,
+      email: email.value,
+      password: password.value
+    }
+    if (usertype.value === 1) {
+      axios.post("localhost:8080/voluntario/registro", body)
+    }
+    if (usertype.value === 2) {
+      axios.post("localhost:8080/institucion/registro", body)
+    }
+  }
+
+</script>
+
+<template>
+  <main>
+    <div class="title">
+      <h1>
+        Bienvenido a la aplicación de Voluntariados de Emegencia
+      </h1>
+      <h2>
+        ¡Regístrate aquí!
+      </h2>
+    </div>
+
+    <div class="form">
+      <input placeholder="Nombre. Ej: René Cáceres" type="text" name="name" id="" v-model="name">
+      <input placeholder="Correo electrónico. Ej: rene@outlook.my" type="text" name="email" id="" v-model="email">
+      <input placeholder="Contraseña" type="text" name="password" id="" v-model="password">
+      <select placeholder="Tipo de usuario" class="tipo-user" v-model="usertype" >
+        <option disabled value="">Selecciona el tipo de usuario</option>
+        <option :value="1">Voluntario</option>
+        <option :value="2">Institución</option>
+      </select>
+      <button @click="sendForm()">
+        Registrarse
+      </button>
+    </div>
+  </main>
+
+
+</template>
+
+
+
+<style scoped>
+template {
+  width: 100vw;
+  height: 100vh;
+}
+
+</style>
