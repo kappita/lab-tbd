bd0dd381c9845e9b75c29fe92cfe198de98f688e
unknown <ignacioladal@gmail.com>
2024-04-21 20:39:21 -0400

    feat: agregar cors para todos los origenes (temporal)

---
+++ backend/src/main/java/Grupo6/VoluntariadoEmergencias/WebConfig/WebConfig.java
@@ -0,0 +1,16 @@
+package Grupo6.VoluntariadoEmergencias.WebConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Especifica el origen permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
