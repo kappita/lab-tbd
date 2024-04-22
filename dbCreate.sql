2e5ac264127c2a02bb2a3ad367a77cf7b7c2003c
unknown <ignacioladal@gmail.com>
2024-04-22 05:45:11 -0400

    feat: agregar correo y contrasena a institucion

--- dbCreate.sql
+++ dbCreate.sql
@@ -1,7 +1,9 @@
 -- Instituciones
 CREATE TABLE Institucion (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
 );
 
 -- Voluntarios
