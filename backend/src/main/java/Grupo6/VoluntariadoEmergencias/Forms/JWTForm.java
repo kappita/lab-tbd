b08acd40405f1684a40b73cec5b83b7b6b31cac9
unknown <ignacioladal@gmail.com>
2024-04-22 02:54:19 -0400

    feat: se agregan objetos para los formularios de inicio de sesion y uso de jwt

---
+++ backend/src/main/java/Grupo6/VoluntariadoEmergencias/Forms/JWTForm.java
@@ -0,0 +1,21 @@
+package Grupo6.VoluntariadoEmergencias.Forms;
+
+public class JWTForm {
+    private String token;
+
+    public JWTForm() {
+        this.token = "";
+    }
+
+    public JWTForm(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
